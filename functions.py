07a0e3b9463fa966420d27b697448ecabced3e19
HYPERPC <nerocross507@gmail.com>
2022-10-07 05:36:16 +0300

    upgrade to hw24

--- functions.py
+++ functions.py
@@ -1,26 +1,29 @@
-def filter_query(param, data):
+import re
+
+
+def filter_query(param: str, data: list[str]) -> list[str]:
     return list(filter(lambda x: param in x, data))
 
 
-def map_query(param, data):
+def map_query(param: str, data: list[str]) -> list[str]:
     col_number = int(param)
-    return map(lambda x: x.split(' ')[col_number], data)
+    return list(map(lambda x: x.split(' ')[col_number], data))
 
 
-def unique_query(data, *args, **kwargs):
+def unique_query(data: list[str], *args: Any, **kwargs: Any) -> list[str]:
     return list(set(data))
 
 
-def sort_query(param, data):
+def sort_query(param: str, data: list[str]) -> list[str]:
     reverse = False if param == 'asc' else True
     return sorted(data, reverse=reverse)
 
 
-def limit_query(param, data):
+def limit_query(param: str, data: list[str]) -> list[str]:
     limit = int(param)
     return list(data[:limit])
 
 
-def regex_query(param, data):
-    pattern = re.compile(param)
+def regex_query(param: str, data: list[str]) -> list[str]:
+    pattern: re.Pattern = re.compile(param)
     return list(filter(lambda x: re.search(pattern, x), data))
